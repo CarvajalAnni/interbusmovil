eff1da88ad77d0b4257d8a6bfe86eabdc1a56856
carvajalanni <carvajalanni@gmail.com>
2022-05-26 12:02:34 -0500

    Interfaz carril cortado

---
+++ app/src/main/java/com/juancamilouni/iterbusmovilidad/Despachador.java
@@ -0,0 +1,14 @@
+package com.juancamilouni.iterbusmovilidad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Despachador extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_despachador);
+    }
+}
